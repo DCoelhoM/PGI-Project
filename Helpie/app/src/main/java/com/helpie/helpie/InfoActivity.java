d79875e8089e8e738b09ad43bf07690e49fa6669
Dinis Coelho Marques <dinis.c.mrqs@gmail.com>
2016-12-12 17:44:14 +0000

    small fix

--- Helpie/app/src/main/java/com/helpie/helpie/InfoActivity.java
+++ Helpie/app/src/main/java/com/helpie/helpie/InfoActivity.java
@@ -23,7 +23,7 @@ public class InfoActivity extends AppCompatActivity {
 
         String ContentStr = String.valueOf(Html
                 .fromHtml("<![CDATA[<body style=\"text-align:justify;color:#f2f2f2;background:#3197d6; \">"
-                        + "    A helpie não se responsabiliza pelo conteúdo dos pedidos feitos, nem pela qualidade dos \"serviços\" prestados, sendo que neste caso os rankings poderão ajudar a prever se uma determinada pessoa é qualificada para o serviço. Também não se responsabiliza pela remuneração dos serviços prestados, apelando à sensatez dos intervenientes para que nao haja um \"lado prejudicado\", uma boa descriçao do serviço e estabelecimento de \"normas\" pode ajudar a diminuir eventuais conflitos entre os intervenientes."
+                        + "    A Helpie não se responsabiliza pelo conteúdo dos pedidos feitos, nem pela qualidade dos \"serviços\" prestados, sendo que neste caso os rankings poderão ajudar a prever se uma determinada pessoa é qualificada para o serviço. Também não se responsabiliza pela remuneração dos serviços prestados, apelando à sensatez dos intervenientes para que não haja um \"lado prejudicado\", uma boa descrição do serviço e estabelecimento de \"normas\" pode ajudar a diminuir eventuais conflitos entre os intervenientes."
                         +"</body>]]>"));
 
         info.loadData(ContentStr, "text/html; charset=utf-8", "utf-8");
